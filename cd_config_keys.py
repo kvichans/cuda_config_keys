7712845f91ae6fd9da5d50349242b17e608c3c9a
Andrey Kvichanskiy <kvichans@mail.ru>
2016-06-24 18:28:22 +0300

    redesign, snips

--- cd_config_keys.py
+++ cd_config_keys.py
@@ -2,18 +2,20 @@
 Authors:
     Andrey Kvichansky    (kvichans on github)
 Version:
-    '0.8.1 2016-05-20'
+    '0.9.1 2016-06-24'
 '''
 #! /usr/bin/env python3
 
 import os, json, re, collections, itertools
+from    fnmatch         import fnmatch
 import  cudatext            as app
+import  cudatext_cmd        as cmds
 from    cudatext        import ed
 import  cudax_lib           as apx
 from    .cd_plug_lib    import *
 from    .cd_keys_report import *
 
-pass;                           LOG     = ( 1==-1)  # Do or dont logging.
+pass;                           LOG     = (-1==-1)  # Do or dont logging.
 pass;                           from pprint import pformat
 pass;                           pf=lambda d:pformat(d,width=150)
 
@@ -22,6 +24,15 @@ _       = get_translation(__file__)
 
 c9, c10, c13    = chr(9), chr(10), chr(13) 
 GAP     = 5
+
+sndt        = None
+try:    # Is plugin cuda_snip2call acitive?
+    from cuda_snip2call import SnipData
+    sndt    = SnipData()
+except ImportError as ex:
+    pass;                       LOG and log('ex={}',(ex))
+    pass
+#sndt        = None
 
 class Command:    
     def dlg_config_keys(self):
@@ -43,29 +54,39 @@ def dlg_config_keys():
         return nkki_l, id2nkks, ks2id
        #def prep_keys_info
     nkki_l, id2nkks, ks2id = prep_keys_info()
+    pass;                      #LOG and log('nkki_l={}',(pf(nkki_l)))
         
-    COL_WS      = [340, 150, 150] 
+    COL_WS      = [340, 200, 200, 150] 
+    COL_WS      = [COL_WS[0]+COL_WS[3]]+COL_WS[1:3]+[0] if not sndt else COL_WS
     LST_W, LST_H= sum(COL_WS)+20, 400
-    DLG_W, DLG_H= GAP+LST_W+GAP, GAP+LST_H+90+GAP+3
+    DLG_W, DLG_H= 5+LST_W+5+100+5, 5+LST_H+105+5+3
+    lfk1        = 5+COL_WS[0]+5
+    lfk2        = 5+COL_WS[0]+COL_WS[1]+5
+    lfsn        = 5+COL_WS[0]+COL_WS[1]+COL_WS[2]+5
+    lrpt        = DLG_W-5-100
     ccnd_h      = _('Suitable command names will contain all specified words.') \
                 +c13+_('Tips:') \
                 +c13+_(' · Use "_" for word boundary.') \
                 +c13+_('   "_up" selects "upper" but not "group".')
-    kcnd_h      = _('Suitable command hotkeys will contain all specified words') \
+    kcnd_h      = _('Suitable command hotkeys will contain all specified words.') \
                 +c13+_('Tips:') \
                 +c13+_(' · Use "_" for key-name boundary.') \
                 +c13+_('   "_f" selects "F1" and "Ctrl+F" but not "Left".')
+    scnd_h      = _('Suitable command snips will match specified string.') \
+                +c13+_('Tips:') \
+                +c13+_(' · Use ? for any character and * for any fragment.')
     addk_h      = _('Set hotkey for command (if no one).'
                   '\rExtend to series hotkeys (if command already has hotkeys).')
     hrpt_h      = _('Shows HTML page with report of all hotkeys in editor.'
                   '\rThe report is usefull to analyze hotkeys in a whole.')
-    trpt_h      = _('Show in new tab compact report of hotkeys in editor')
+    trpt_h      = _('Show compact report of hotkeys in new tab.'
+                  '\rThe report is usefull to find free hotkeys.')
     cpnm_h      = _('Copy command name to clipboard')
     reND    = re.compile(r'\W')
-    def prep_nm(nm):
-        nm  = nm.replace('&', '')
-        nm  = nm.replace('\\', ': ')
-        return nm
+    def is_cond4snps(cond, sns_l):
+        if not cond:    return  True
+        if not sns_l:   return  False
+        return any(map(lambda sn:fnmatch(sn, cond), sns_l))
     def test_cond(cnd_s, text, text2='', what='cmds'):
         if not cnd_s:       return True
         if not text+text2:  return False
@@ -77,46 +98,77 @@ def dlg_config_keys():
             cnd_s   = cnd_s.replace(' _', ' ·').replace('_ ', '· ')
         pass;                  #LOG and log('cnd_s, text={}',(cnd_s, text))
         return all(map(lambda c:c in text, cnd_s.split()))
+    def bmix(bor12, bor23, val1, val2, val3):
+        val12   = val1  or val2     if bor12 else        val1  and val2
+        return    val12 or val3     if bor23 else        val12 and val3
 #   cmd_id      = 524391
     cmd_id      = ''
     ccnd        = ''
     kcnd        = ''
+    scnd        = ''
+    orcn        = False
+    orsn        = False
     focused     = 'ccnd'
     while True:
         ccnd_u  = ccnd.upper()
         kcnd_u  = kcnd.upper()
-        fl_NKKIs= [    (prep_nm(nm), k1, k2, id)
-                   for (nm,          k1, k2, id)   in nkki_l
-                   if  test_cond(ccnd_u, nm)
-                   and test_cond(kcnd_u, k1, k2, 'keys')]
-        stat    = f(' ({}/{})',len(fl_NKKIs), len(nkki_l)) #if len(fl_NKKIs)==en(nkki_l)
-        fl_Is   = [id         for (nm, k1, k2, id) in fl_NKKIs ]
-        itms    = (zip([_('Command')+stat, _('Hotkey-1'), _('Hotkey-2')], map(str, COL_WS))
-                  ,    [ (nm,                 k1,             k2) for (nm, k1, k2, id) in fl_NKKIs ])
-        cnts    =[dict(           tp='lb'   ,tid='ccnd' ,l=GAP          ,w=90   ,cap=_('In &Command:')          ,hint=ccnd_h) # &c
-                 ,dict(cid='ccnd',tp='ed'   ,t=GAP      ,l=GAP+90       ,w=150                                              ) #
-                 ,dict(           tp='lb'   ,tid='kcnd' ,l=GAP+270      ,w=50   ,cap=_('In &Hotkeys:')          ,hint=kcnd_h) # &h
-                 ,dict(cid='kcnd',tp='ed'   ,t=GAP      ,l=GAP+350      ,w=150                                              ) #
-                 ,dict(cid='lwks',tp='lvw'  ,t=GAP+30   ,l=GAP          ,w=LST_W,h=LST_H  ,items=itms ,props='1'            ) #     grid
-                 ,dict(cid='fltr',tp='bt'   ,tid='ccnd' ,l=GAP+500      ,w=80   ,cap=_('&Filter list'),props='1'            ) # &f  default
-                 ,dict(cid='drop',tp='bt'   ,tid='kcnd' ,l=GAP+LST_W-80 ,w=80   ,cap=_('Show &all')                         ) # &a
-                 ,dict(cid='add1',tp='bt'   ,t=DLG_H-60 ,l=GAP          ,w=150  ,cap=_('Set/Add Hotkey-&1')     ,hint=addk_h) # &1
-                 ,dict(cid='add2',tp='bt'   ,t=DLG_H-30 ,l=GAP          ,w=150  ,cap=_('Set/Add Hotkey-&2')     ,hint=addk_h) # &2
-                 ,dict(cid='del1',tp='bt'   ,t=DLG_H-60 ,l=GAP+150+GAP  ,w=150  ,cap=_('Remove Hotkey-1 &!')                ) # &!
-                 ,dict(cid='del2',tp='bt'   ,t=DLG_H-30 ,l=GAP+150+GAP  ,w=150  ,cap=_('Remove Hotkey-2 &@')                ) # &@
-                 ,dict(cid='cpnm',tp='bt'   ,t=DLG_H-60 ,l=GAP+300+GAP*2,w=110  ,cap=_('Copy &name')            ,hint=cpnm_h) # &n 
-                 ,dict(cid='hrpt',tp='bt'   ,t=DLG_H-60 ,l=DLG_W-GAP-200,w=110  ,cap=_('HT&ML-report')          ,hint=hrpt_h) # &m 
-                 ,dict(cid='trpt',tp='bt'   ,t=DLG_H-30 ,l=DLG_W-GAP-200,w=110  ,cap=_('&Tab-report')           ,hint=trpt_h) # &t
-                 ,dict(cid='help',tp='bt'   ,t=DLG_H-60 ,l=DLG_W-GAP-80 ,w=80   ,cap=_('Hel&p')                             ) # &p 
-                 ,dict(cid='-'   ,tp='bt'   ,t=DLG_H-30 ,l=DLG_W-GAP-80 ,w=80   ,cap=_('Close')                             ) #  
-                ]
+        pass;                  #LOG and log('ccnd_u, kcnd_u, scnd={}',(ccnd_u, kcnd_u, scnd))
+        nkkis_l = [    (nm, k1, k2, id, sndt.get_snips(id) if sndt else [])
+                   for (nm, k1, k2, id)   in nkki_l]
+        pass;                  #LOG and log('nkkis_l={}',(pf(nkkis_l)))
+        fl_NKKISs=[    (nm, k1, k2, id, sns)
+                   for (nm, k1, k2, id, sns)   in nkkis_l
+                   if  bmix(orcn, orsn
+                                  ,(not ccnd_u or               test_cond(ccnd_u, nm))
+                                  ,(not kcnd_u or               test_cond(kcnd_u, k1, k2, 'keys'))
+                                  ,(not scnd   or not sndt   or is_cond4snps(scnd, sns)) )]
+        stat    = f(' ({}/{})',len(fl_NKKISs), len(nkkis_l))
+        fl_Is   = [id         for (nm, k1, k2, id, sn) in fl_NKKISs ]   ##!!
+        itms    = (zip([_('Command')+stat, _('Hotkey-1'), _('Hotkey-2'), _('Snip')], map(str, COL_WS))
+                  ,    [ (nm,                 k1,             k2,          ', '.join(sns)) 
+                        for  (nm, k1, k2, id, sns) in fl_NKKISs ])
+        cnts    =([]
+                 +[dict(cid='fltr',tp='bt'  ,t=5+40+5   ,l=lrpt ,w=100  ,cap=_('&Filter')       ,props='1'          )] # &f  default
+                 +[dict(cid='drop',tp='bt'  ,t=5+70+5   ,l=lrpt ,w=100  ,cap=_('&All')                              )] # &a
+                 +[dict(cid='orcn',tp='ch'  ,t=5        ,l=lfk1-40,w=40 ,cap=_('&OR')           ,act='1'            )] # &o
+                +([] if not sndt else []
+                 +[dict(cid='orsn',tp='ch'  ,t=5        ,l=lfsn-40,w=40 ,cap=_('O&R')           ,act='1'            )] # &r
+                )
+                 +[dict(           tp='lb'  ,tid='orcn' ,l=5+5  ,w=90   ,cap=_('In &Command:')          ,hint=ccnd_h)] # &c
+                 +[dict(cid='ccnd',tp='ed'  ,t=5+20     ,l=5+5  ,w=150                                              )] #
+                 +[dict(           tp='lb'  ,tid='orcn' ,l=lfk1 ,w=50   ,cap=_('In &Hotkeys:')          ,hint=kcnd_h)] # &h
+                 +[dict(cid='kcnd',tp='ed'  ,t=5+20     ,l=lfk1 ,w=120                                              )] #
+                +([] if not sndt else []
+                 +[dict(           tp='lb'  ,tid='orsn' ,l=lfsn ,w=50   ,cap=_('In &Snip(s):')          ,hint=scnd_h)] # &s
+                 +[dict(cid='scnd',tp='ed'  ,t=5+20     ,l=lfsn ,w=100                                              )] #
+                )
+                 +[dict(cid='lwks',tp='lvw' ,t=5+40+5   ,l=5    ,w=LST_W,h=LST_H  ,items=itms   ,props='1'          )] #     grid
+                
+                 +[dict(cid='cpnm',tp='bt'  ,t=DLG_H-60 ,l=5+5  ,w=110  ,cap=_('Copy &name')            ,hint=cpnm_h)] # &n 
+                 +[dict(cid='add1',tp='bt'  ,t=DLG_H-60 ,l=lfk1 ,w=150  ,cap=_('Set/Add Hotkey-&1')     ,hint=addk_h)] # &1
+                 +[dict(cid='del1',tp='bt'  ,t=DLG_H-30 ,l=lfk1 ,w=150  ,cap=_('Remove Hotkey-1 &!')                )] # &!
+                 +[dict(cid='add2',tp='bt'  ,t=DLG_H-60 ,l=lfk2 ,w=150  ,cap=_('Set/Add Hotkey-&2')     ,hint=addk_h)] # &2
+                 +[dict(cid='del2',tp='bt'  ,t=DLG_H-30 ,l=lfk2 ,w=150  ,cap=_('Remove Hotkey-2 &@')                )] # &@
+                +([] if not sndt else []
+                 +[dict(cid='asnp',tp='bt'  ,t=DLG_H-60 ,l=lfsn ,w=150  ,cap=_('Set/A&dd Snip')                     )] # &d
+                 +[dict(cid='rsnp',tp='bt'  ,t=DLG_H-30 ,l=lfsn ,w=150  ,cap=_('R&emove Snip(s)')                   )] # &e
+                )
+                 +[dict(cid='hrpt',tp='bt'  ,t=DLG_H-120,l=lrpt ,w=100  ,cap=_('In HT&ML')              ,hint=hrpt_h)] # &m 
+                 +[dict(cid='trpt',tp='bt'  ,t=DLG_H-90 ,l=lrpt ,w=100  ,cap=_('In &Tab')               ,hint=trpt_h)] # &t
+                 +[dict(cid='help',tp='bt'  ,t=DLG_H-60 ,l=lrpt ,w=100  ,cap=_('Hel&p')                             )] # &p 
+                 +[dict(cid='-'   ,tp='bt'  ,t=DLG_H-30 ,l=lrpt ,w=100  ,cap=_('Close')                             )] #  
+                )
         lwks_n  = -1    if 0==len(fl_Is)        else \
                    0    if cmd_id not in fl_Is  else \
                    fl_Is.index(cmd_id)
-        pass;                   LOG and log('lwks_n={}',(lwks_n))
-        vals    = dict(ccnd=ccnd
-                      ,kcnd=kcnd
-                      ,lwks=lwks_n)
+        pass;                  #LOG and log('lwks_n={}',(lwks_n))
+        vals    =       dict(ccnd=ccnd
+                            ,kcnd=kcnd
+                            ,orcn=orcn
+                            ,orsn=orsn
+                            ,lwks=lwks_n)
+        if sndt:
+            vals.update(dict(scnd=scnd))
         pass;                  #LOG and log('in-vals={}',(vals))
         btn, vals, chds = dlg_wrapper(_('Config Keymap'), DLG_W, DLG_H, cnts, vals, focus_cid=focused)
         pass;                  #LOG and log('an-vals={}',(vals))
@@ -124,17 +176,21 @@ def dlg_config_keys():
         if btn is None or btn=='-':    return#while True
         ccnd    = vals['ccnd'].strip()
         kcnd    = vals['kcnd'].strip()
+        scnd    = vals['scnd'].strip() if sndt else ''
+        orcn    = vals['orcn']
+        orsn    = vals['orsn']
         lwks_n  = vals['lwks']
         cmd_id  = '' if lwks_n==-1 else fl_Is[lwks_n]
-        pass;                   LOG and log('cmd_id={}',(cmd_id,))
+        pass;                  #LOG and log('cmd_id={}',(cmd_id,))
         focused = chds[0] if 1==len(chds) else focused
-        pass;                   LOG and log('chds, focused={}',(chds, focused))
+        pass;                  #LOG and log('chds, focused={}',(chds, focused))
         if False:pass
-#       elif btn in 'fltr':
-#           focused = chds[0] if 1==len(chds) else 'ccnd'
+        elif btn in 'fltr':
+            focused = 'lwks'
         elif btn=='drop':
             ccnd    = ''
             kcnd    = ''
+            scnd    = ''
         
         elif btn=='trpt':
             # Compact report to tab
@@ -176,7 +232,11 @@ def dlg_config_keys():
             if new_k in ks2id:
                 dbl_id  = ks2id[new_k]
                 dbl_nkk = id2nkks[dbl_id]
-                if app.msg_box(f(_('Hotkey "{}" is already assigned \nto command "{}".\n\nDo you want to reassign the hotkey \nto selected command "{}"?')
+                if app.msg_box(f(_('Hotkey "{}" is already assigned '
+                                   '\nto command "{}".'
+                                   '\n'
+                                   '\nDo you want to reassign the hotkey '
+                                   '\nto selected command "{}"?')
                                 , new_k, dbl_nkk[0], cmd_nkk[0]), app.MB_OKCANCEL)==app.ID_CANCEL: continue#while
                 dbl_i   = 1 if dbl_nkk[1]==new_k else 2
                 pass;           LOG and log('dbl_id, dbl_nkk={}',(dbl_id, dbl_nkk))
@@ -193,6 +253,37 @@ def dlg_config_keys():
             if not set_ok:  log('Fail to use PROC_SET_HOTKEY for cmd "{}"', cmd_id)
             nkki_l, id2nkks, ks2id = prep_keys_info()
         
+        elif btn=='rsnp' and cmd_id and sndt: 
+            cnm     = sndt.get_name(cmd_id)
+            snp_l   = sndt.get_snips(cmd_id)
+            snps    = ', '.join(snp_l)
+            if app.msg_box(f(_('Do you want to remove snip(s) '
+                               '\n    {}'
+                               '\nfor command "{}"?')
+                            , snps, cnm), app.MB_OKCANCEL)==app.ID_CANCEL: continue#while
+            for snp in snp_l:
+                sndt.free(snp)
+                
+        elif btn=='asnp' and cmd_id and sndt:
+            cnm     = sndt.get_name(cmd_id)
+            new_sn  = app.dlg_input(f(_('Add snip for "{}"'), cnm), '') 
+            if not new_sn:  continue#while
+            while not SnipData.is_snip(new_sn):
+                app.msg_status(SnipData.msg_correct_snip)
+                new_sn  = app.dlg_input(f(_('Snip for "{}"'), cnm), new_sn) 
+                if not new_sn:  break
+            if not new_sn:  continue#while
+            pre_cid = sndt.get_cmdid(new_sn)
+            if pre_cid:
+                pre_cnm = sndt.get_name(pre_cid)
+                if app.msg_box(f(_('Snip "{}" is already assigned '
+                                   '\nto command "{}".'
+                                   '\n'
+                                   '\nDo you want to reassign the snip '
+                                   '\nto command "{}"?')
+                                , new_sn, pre_cnm, cnm), app.MB_OKCANCEL)==app.ID_CANCEL: continue#while
+            sndt.set(new_sn, cmd_id)
+
         elif btn=='help':
             DW, DH      = DLG_W-2*GAP, DLG_H-2*GAP
             dlg_wrapper(_('Help for "Config Keymap"'), DLG_W, DLG_H,
@@ -205,6 +296,11 @@ def dlg_config_keys():
                        +c13+_('• In Hotkeys. ')
                        +c13+kcnd_h
                        +c13+' '
+                       +('' if not sndt else ''
+                       +c13+_('• In Snip. ')
+                       +c13+scnd_h
+                       +c13+' '
+                       )
                        +c13+_('• Set/Add. ')
                        +c13+addk_h
                        +c13+' '
@@ -257,7 +353,7 @@ def collect_keys():
         dct_keys= keys.get(plug_id, {})
         
         cmdinfos += [('Plugins'
-                    , 'plugin: '+cap
+                    , 'plugin: '+cap.replace('&', '').replace('\\', ': ')
                     , ' * '.join(dct_keys.get('s1', []))
                     , ' * '.join(dct_keys.get('s2', []))
                     , f('{},{},{}', modul, meth, par).rstrip(',')
@@ -274,4 +370,6 @@ if __name__ == '__main__':
 [+][kv-kv][11dec15] Init
 [+][kv-kv][10may16] Shift HK-2 to HK-1 after del HK-1
 [ ][kv-kv][10may16] @ in "In cmd" for filter in keys
+[ ][kv-kv][27may16] Remove & to filter
+[ ][kv-kv][27may16] Force unhide cmd after RemoveK 
 """
